c7a5a97e34fe596a9f2b4764303fcd71132d66d4
youssef Shaban <85808789+youssef-shaban@users.noreply.github.com>
2022-11-03 21:48:59 +0200

    Adding modes json

--- app.py
+++ app.py
@@ -5,28 +5,35 @@ from scipy.fft import rfft, irfft
 import numpy as np
 import altair as alt
 import pandas as pd
-import time
-from utils import equalizer, mode1
+import json
+from utils import equalizer
 
 st.set_page_config(
     page_title="Equalizer",
     page_icon="🔊",
     layout="wide"
 )
-# with open("style.css") as design:
-#     st.markdown(f"<style>{design.read()}</style>", unsafe_allow_html=True)
+with open("modes.json") as infile:
+    data = json.load(infile)
 if "is_uploaded" not in st.session_state:
     st.session_state.is_uploaded=True
-graph_container= st.container()
-returned_signal= np.zeros(1000)
 
+returned_signal= np.zeros(1000)
 if "time" not in st.session_state:
     st.session_state.time=np.linspace(0,5,1000)
 
-uploader= st.file_uploader("upload wav")
+
+col1, col2= st.columns([0.7,2])
+with col1:
+    mode=st.selectbox("Mode",data["modes"])
+    uploader= st.file_uploader("upload wav")
+
+col5, col6,_,col7= st.columns([0.7,0.7,1.2,0.1])
 
 if uploader:
-    st.audio(uploader)
+    with col5:
+        st.write("Original Sound")
+        st.audio(uploader)
     if st.session_state.is_uploaded:
         sample_rate, signal= read(uploader)
         time=signal.shape[0]/sample_rate
@@ -43,22 +50,23 @@ if uploader:
             gain_list.append(st.session_state[f"slider{i}"])
         except:
             gain_list.append(0)
-    transformed= equalizer(mode1,st.session_state["transformed_signal"],st.session_state["points_per_freq"],gain_list)
+    transformed= equalizer(data[mode]["sliders"],st.session_state["transformed_signal"],st.session_state["points_per_freq"],gain_list)
     returned_signal= np.asarray(irfft(transformed), dtype=np.int16)
     modified_audio=write("clean.wav",st.session_state["sample_rate"], returned_signal)
 else:
     st.session_state.is_uploaded=True
+    st.session_state.time=np.linspace(0,5,1000)
     
 
-cols= st.columns([ 1 for i in range(10)])
+cols= st.columns([ 1 for i in range(data[mode]["num_sliders"])])
 for index,i in enumerate(cols):
     with i:
         svs.vertical_slider(key=f"slider{index}",default_value=0.0,min_value=-12.0,max_value=12.0,step=0.1, )
-# for i in range(10):
-#     st.write(st.session_state[f"slider{i}"])
 
 if uploader:
-    st.audio("clean.wav")
+    with col6:
+        st.write("Modified sound")
+        st.audio("clean.wav")
 
 ploted_data= pd.DataFrame({"time":st.session_state.time[::300],"signal":returned_signal[::300]})
 df=ploted_data.iloc[0:1500]
@@ -82,8 +90,8 @@ def plot_animation(df):
 
 N = ploted_data.shape[0] 
 burst = 1500       
-line_plot= graph_container.altair_chart(lines,use_container_width=True)
-start_btn = graph_container.button('Start')
+line_plot= col2.altair_chart(lines,use_container_width=True)
+start_btn = col7.button('Start')
 
 if start_btn:
     for i in range(1500,N-burst):
@@ -92,4 +100,3 @@ if start_btn:
         line_plot.altair_chart(lines,use_container_width=True)
         
         
-# line_plot = st.altair_chart(lines)
